db2af12d0e4332b023ae1c4954e4f980e916bad7
mohammed hammami <mohammed.hammami@hotmail.com>
2024-04-07 02:47:28 -0900

    backend correct conniction

--- backend/index.js
+++ backend/index.js
@@ -9,6 +9,8 @@ const PORT = process.env.PORT || 5000;
 const postsRouter = require("./routes/posts");
 const categoriesRouter = require("./routes/Categories");
 const ColorRouter = require("./routes/Color");
+const OrderRouter = require("./routes/Orders");
+const ItemRouter = require("./routes/Orders");
 
 app.use(cors());
 app.use(express.json());
@@ -18,6 +20,8 @@ app.use(express.json());
 app.use("/posts", postsRouter);
 app.use("/categories", categoriesRouter);
 app.use("/color", ColorRouter);
+app.use("/order", OrderRouter);
+app.use("/item", ItemRouter);
 
 // Handles any other endpoints [unassigned - endpoints]
 app.use("*", (req, res) => res.status(404).json("NO content at this path"));
